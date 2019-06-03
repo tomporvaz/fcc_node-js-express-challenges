0aa2f96372510f852ec4867b6c9c0991d4d0b776
tomporvaz <tomporvaz@gmail.com>
2019-06-03 18:58:54 -0400

    Made public directory public with express.static.

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ console.log("Hello World");
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 /** 4) Serve static assets  */
-
+app.use("/", () => express.static(__dirname + "/public")); 
 
 /** 5) serve JSON on a specific route */
 
