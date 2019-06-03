87a45d300c20128d59bfb8ce753dbe0420559d18
tomporvaz <tomporvaz@gmail.com>
2019-06-03 19:47:05 -0400

    Added uppercase value to .env, and added logic to json response handler.

--- myApp.js
+++ myApp.js
@@ -21,10 +21,14 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public")); 
 
 /** 5) serve JSON on a specific route */
-app.get("/json", (req, res) => res.json({"message": "Hello json"}));
+app.get("/json", (req, res) => {
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message": "HELLO JSON"}) 
+    } else {res.json({"message": "Hello json"})}
+});
 
 /** 6) Use the .env file to configure the app */
- 
+process.env.MESSAGE_STYLE=uppercase;
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
