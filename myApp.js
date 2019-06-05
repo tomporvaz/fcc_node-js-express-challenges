731bf50544d9019e9393a3d93e2d674da341f93e
tomporvaz <tomporvaz@gmail.com>
2019-06-05 18:25:54 -0400

    Added route for echo using word param.

--- myApp.js
+++ myApp.js
@@ -50,7 +50,7 @@ function jsonDate(req, res){
 
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) => {res.json({"echo": res.params.word})});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
