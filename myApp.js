d743fef0e2cb857147ae8410312f330b31fa47a5
tomporvaz <tomporvaz@gmail.com>
2019-06-06 16:51:51 -0400

    Added post route to /name to return json object.

--- myApp.js
+++ myApp.js
@@ -55,7 +55,9 @@ app.get("/:word/echo", (req, res) => {res.json({"echo": req.params.word})});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-app.get("/name", (req, res) => res.json({"name": req.query.first + " " + req.query.last}));
+app.route("/name")
+.get((req, res) => res.json({"name": req.query.first + " " + req.query.last}))
+.post((req, res) => res.json({"name": req.body.first + " " + req.body.last}));
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
